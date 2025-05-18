6358e38d2bcf87ffe2178553c406f0c7731cd7d5
nicol <nicolas.mahn@gmx.de>
2025-05-18 18:14:10 +0200

    v1.4.6 removed unused secret

--- scrt.py
+++ scrt.py
@@ -1,7 +1,6 @@
 import os
 
 CHROMADB_HOST = os.getenv("CHROMADB_HOST")
-REDIS_HOST = os.getenv("REDIS_HOST")
 CHROMADB_PORT = int(os.getenv("CHROMADB_PORT"))
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 GOOGLE_KEY = os.getenv("GOOGLE_KEY")
